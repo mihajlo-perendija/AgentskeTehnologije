b8b540bdeedc65a676d57957169ea13c0b13a91e
mihajlo-perendija <54019549+mihajlo-perendija@users.noreply.github.com>
2020-04-16 00:44:01 +0200

    Initial frontend commit

---
+++ chat-client/copy-build.bat
@@ -0,0 +1,1 @@
+xcopy /s "H:\FTN\8. semestar\Agentske tehnologije\GitRepo\AgentskeTehnologije\chat-client\build" "H:\FTN\8. semestar\Agentske tehnologije\GitRepo\AgentskeTehnologije\ChatWar\WebContent"
